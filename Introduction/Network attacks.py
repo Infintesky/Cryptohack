0992187d2f46ed1c3c453f8651d4e55bc884bfb3
Infintesky <weiyang2912@hotmail.com>
2021-01-17 23:08:27 +0800

    Create Network attacks.py

---
+++ Introduction/Network attacks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import telnetlib
+import json
+
+HOST = "socket.cryptohack.org"
+PORT = 11112
+
+tn = telnetlib.Telnet(HOST, PORT)
+
+# function to read line returned from server
+def readline():
+    return tn.read_until(b"\n")
+
+def json_recv():
+    line = readline()
+    return json.loads(line.decode())
+
+def json_send(hsh):
+    request = json.dumps(hsh).encode()
+    tn.write(request)
+
+print(readline())
+print(readline())
+print(readline())
+print(readline())
+
+request = {
+    "buy": "flag"
+}
+
+json_send(request)
+response = json_recv()
+print(response)
